b002a65ee017c4aa2cf33c69c068c7929dcbd38e
Jakub Bednarczyk <122677822+sloneczny40@users.noreply.github.com>
2024-01-13 15:55:20 +0100

    ending game

--- main.py
+++ main.py
@@ -4,42 +4,44 @@ import random
 
 def find_bigger(number1, number2):
     if number1>number2:
-        return True
+        return 'a'
     elif number2<number2:
-        return False
+        return 'b'
     
 print(assets.logo)
 
-score = 0
-end_game = False
 
-while end_game is False:
-    random_numbers = []
-    for i in range(2):
-        number = random.randint(0, len(data)-1)
-        random_numbers.append(number)
-    followers_a = data[random_numbers[0]]['follower_count']
-    followers_b = data[random_numbers[1]]['follower_count']
+def game():
+    end_game = False
 
-    print(f" A: {data[random_numbers[0]]['name']}, a {data[random_numbers[0]]['description']} from {data[random_numbers[0]]['country']}")
-    print(assets.vs)
-    print(f" B: {data[random_numbers[1]]['name']}, a {data[random_numbers[1]]['description']} from {data[random_numbers[1]]['country']}")
+    score = 0
 
-    print(f"___test_only_______{followers_a} vs {followers_b}")
+    while end_game is False:
+        
+        random_numbers = []
 
-    player_choice = input("Who has more followers? type 'a' or 'b': ")
-    if player_choice=='a':
-        if find_bigger(followers_a, followers_b):
-            print("\n"*10)
-            print(f"GOOD! Your score is {score}")
-            score +=1
+        for i in range(2):
+            number = random.randint(0, len(data)-1)
+            random_numbers.append(number)
+
+        followers_a = data[random_numbers[0]]['follower_count']
+        followers_b = data[random_numbers[1]]['follower_count']
+
+        print(f" A: {data[random_numbers[0]]['name']}, a {data[random_numbers[0]]['description']} from {data[random_numbers[0]]['country']}")
+        print(assets.vs)
+        print(f" B: {data[random_numbers[1]]['name']}, a {data[random_numbers[1]]['description']} from {data[random_numbers[1]]['country']}")
+
+        player_choice = input("Who has more followers? type 'a' or 'b': ").lower()
+        # player_choice = 'a'
+        if player_choice == find_bigger(followers_a, followers_b):
+            
+                score +=1
+                print(f"GOOD! Your score is {score}")
+        
         else:
+            print(f"\nSorry, thats wrong answer.\nYour final score is: {score}\n")
             end_game = True
-    elif player_choice=='b':
-        if not find_bigger(followers_a, followers_b):
-            print("\n"*10)
-            print(f"GOOD! Your score is {score}")
-            score += 1
-        else:
-            end_game = True
-print(f"\nYour final score is: {score}")
+    
+
+
+game()
